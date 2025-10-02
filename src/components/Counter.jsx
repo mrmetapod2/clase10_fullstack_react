21cc035cfd9e62ed5fc677300449a9ce410810ff
Ian Badino <ianlucab@hotmail.com.ar>
2025-10-02 08:42:46 -0300

    boton reset para Counter

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,20 +2,25 @@ import { useState } from 'react'
 import CustomButton from './CustomButton';
 
 export default function Counter() {
-      const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
   return (
     <div>
-      <CustomButton onClick={() => count > 0 ? setCount((count) => count - 1) : alert("No se puede decrementar más")} 
-      btnText="Decrementar" 
-      variant="danger" 
-      size="small" 
-      disabled={false}/>
+      <CustomButton onClick={() => count > 0 ? setCount((count) => count - 1) : alert("No se puede decrementar más")}
+        btnText="Decrementar"
+        variant="danger"
+        size="small"
+        disabled={false} />
       <p>El contador está en {count}</p>
-      <CustomButton onClick={() => count < 10 ? setCount((count) => count + 1) : alert("No se puede incrementar más")} 
-      btnText="Incrementar" 
-      variant="primary" 
-      size="small" 
-      disabled={false}/>
+      <CustomButton onClick={() => count < 10 ? setCount((count) => count + 1) : alert("No se puede incrementar más")}
+        btnText="Incrementar"
+        variant="primary"
+        size="small"
+        disabled={false} />
+      <CustomButton onClick={() => setCount(0)}
+        btnText="Resetear"
+        variant="primary"
+        size="small"
+      />
     </div>
   );
 }
